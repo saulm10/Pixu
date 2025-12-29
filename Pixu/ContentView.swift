2ddb2434be2c471f928ba3f1130df866d690e467
SaulMac <sulomd31@gmail.com>
2025-12-29 15:25:35 +0100

    Initial Commit

---
+++ Pixu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pixu
+//
+//  Created by Saul Martinez Diez on 29/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pixu/PixuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixuApp.swift
+//  Pixu
+//
+//  Created by Saul Martinez Diez on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PixuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
